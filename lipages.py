836d4c73bed7fa7ec0a4504cf06af934c16bf02d
Lip <truthsetone@outlook.com>
2024-05-27 18:34:38 +0800

    update and add

--- lipages.py
+++ lipages.py
@@ -1,10 +1,10 @@
-'''
+print('''
 linked pages, very personal blog script. use at YOUR OWN RISK!!!
-> python lipages.py add 2023-12-22-新文章.md "新文章标题" --author "作者名" --tags "标签1" --tags "标签2"
-> python lipages.py dlt 2023-12-22-新文章.md
+> python lipages.py add 2023-12-22-新文章.md "新文章标题" --author "作者名" --tag "标签1" --tag "标签2"
+> python lipages.py rmv 2023-12-22-新文章.md
 > python lipages.py fix 2023-12-22-新文章.md "新的标题"
 > python lipages.py lst
-'''
+''')
 
 import os, re
 import click as cli
@@ -18,7 +18,7 @@ def ensure_blog_dir():
     if not os.path.exists(BLOG_DIR):
         os.makedirs(BLOG_DIR)
 
-def create_blog_file(filename, title, author, tags):
+def create_blog_file(filename, title, author, tag):
     date_pattern = r'\d{4}[-/]\d{2}[-/]\d{2}|\d{2}[-/]\d{2}[-/]\d{4}|\w{3}\s\d{1,2},?\s\d{4}'
     match = re.search(date_pattern, filename)
     if match: date_strbgn, date_strend = match.span()
@@ -26,14 +26,14 @@ def create_blog_file(filename, title, author, tags):
     #
     ensure_blog_dir()
     filepath = os.path.join(BLOG_DIR, filename)
-    if os.path.exists(filepath): return (f'ERROR {filepath} already exists! Not added!')
+    if os.path.exists(filepath): return (f'__ERROR__: {filepath} already exists! Not added!')
     #
     with open(filepath, 'w', encoding='utf-8') as f:
         f.write("---\n") # line 0  # 这里不要随意增删条目, 否则后面fix/lst处理文章标题时就会出错
         f.write(f"Title: {title}\n")
         f.write(f"Date: {the_date}\n") # 需要判断...
         f.write(f"Author: {author}\n")
-        f.write(f"Tags: [{', '.join(tags)}]\n")
+        f.write(f"Tags: [{', '.join(tag)}]\n")
         f.write("---\n")
         f.write("\n") # line 6
         f.write(f"# {title}\n")
@@ -48,7 +48,7 @@ def update_index(action, filename, title=None):
     if action == 'add':
         new_line = f"- [{title}](blogs/{filename})\n"
         lines.append(new_line)
-    elif action == 'dlt':
+    elif action == 'rmv':
         lines = [line for line in lines if f"blogs/{filename}" not in line]
 
     with open(BLOG_INDEX, 'w', encoding='utf-8') as f: f.writelines(lines)
@@ -60,25 +60,25 @@ def lipages(): pass
 @cli.argument('filename')
 @cli.argument('title')
 @cli.option('--author', default=BLOG_AUTHOR, help='The author of the blog post')
-@cli.option('--tags', multiple=True, help='Tags for the blog post')
-def add(filename, title, author, tags):
+@cli.option('--tag', multiple=True, help='Tag for the blog post, can appear many times')
+def add(filename, title, author, tag):
     """Add (initialize) a new blog post."""
-    filepath = create_blog_file(filename, title, author, tags)
-    if filepath.startswith('ERROR'):
-        cli.echo('Not added for the file already exists!')
+    filepath = create_blog_file(filename, title, author, tag)
+    if filepath.startswith('__ERROR__: '):
+        cli.echo(f'Not added for the file `{filename}` already exists!')
     else:
         the_path = update_index('add', filename, title)
         cli.echo(f"Created new blog post at {filepath} and updated {BLOG_INDEX}")
 
 @lipages.command()
 @cli.argument('filename')
-def dlt(filename):
-    """Delete a blog post."""
+def rmv(filename):
+    """Remove a blog post."""
     filepath = os.path.join(BLOG_DIR, filename)
     if os.path.exists(filepath):
         os.remove(filepath)
-        update_index('dlt', filename)
-        cli.echo(f"Deleted blog post {filename} and updated {BLOG_INDEX}")
+        update_index('rmv', filename)
+        cli.echo(f"Removed blog post {filename} and updated {BLOG_INDEX}")
     else:
         cli.echo(f"File {filename} does not exist")
 
@@ -96,7 +96,7 @@ def fix(filename, new_title):
             f.seek(0)
             f.writelines(lines)
 
-        update_index('dlt', filename)
+        update_index('rmv', filename)
         update_index('add', filename, new_title)
         cli.echo(f"Modified blog post {filename} title to {new_title} and updated {BLOG_INDEX}")
     else:
